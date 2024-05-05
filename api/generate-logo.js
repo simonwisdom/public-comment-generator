776593de3ed2ebf436a1585732b26c92fe56e106
Simon Wisdom <simonowisdom@gmail.com>
2024-05-04 22:48:34 -0600

    prompt input

--- api/generate-logo.js
+++ api/generate-logo.js
@@ -41,9 +41,9 @@ export default async function handler(req, res) {
       // Generate logo image using Replicate API
       const replicate = new Replicate();
       const input = {
-        prompt: logoDescription,
+        prompt: `A logo based on the following description: ${logoDescription}`,
         scheduler: "K_EULER"
-      };
+      };      
 
       const output = await replicate.run("stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4", { input });
 
