053e239fff3838aa2bf612c54fc9174859a05d81
Simon Wisdom <simonowisdom@gmail.com>
2024-05-05 12:12:48 -0600

    added page title

--- src/App.js
+++ src/App.js
@@ -145,6 +145,7 @@ const App = () => {
 
   return (
     <div className="container">
+      <h1 style={{ fontSize: '24px', fontWeight: 'bold' }}>Generate a high quality PDF for public comment</h1>
       <h1>Enter Document Number (in the form XXXX-XXXXX)</h1>
       <input className="input" type="text" value={documentNumber} onChange={handleDocumentNumberChange} />
       <button className="button" onClick={handleSummarizeDoc} disabled={isLoading}>
